c16b195ffcdafb4f11111edd2c2af0c1f1571b43
alex <talexroger1997@gmail.com>
2023-11-02 23:37:38 -0500

    PRIMER COMMIT BASE

---
+++ script.js
@@ -0,0 +1,2 @@
+// Puedes agregar código JavaScript aquí si deseas agregar interactividad a tu página web.
+// Por ejemplo, podrías utilizar JavaScript para manipular elementos del DOM o realizar acciones específicas cuando los usuarios interactúen con la página.
